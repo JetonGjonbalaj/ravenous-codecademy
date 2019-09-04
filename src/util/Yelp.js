af097e07b92815700476a26a1c8361a6853ddb2f
Jeton Gjonbalaj <jetonmgjonbalaj@gmail.com>
2019-09-04 21:27:46 +0200

    added yelp api

---
+++ src/util/Yelp.js
@@ -0,0 +1,42 @@
+const apiKey = '5R9oauDyKpWsdUP4x3VZZeZtL-Owd7Ix7v7i5rygaWdqzKQvVlJD1-_WozkHA8Cw8CGD9QttRFjfu-wNf0TKEyHCEF9wUnB-Pb5DwPHi4WpwMtTnkI7jUzItRQJwXXYx';
+
+const Yelp = {
+    search: function(term, location, sortBy) {
+        const h = new Headers();
+        h.append("Authorization", "Bearer " + apiKey);
+        let url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=50&term=${term}&location=${location}&sort_by=${sortBy}`;
+        return fetch(
+            url,
+            {
+                "x-requested-with": "xmlhttprequest",
+                "Access-Control-Allow-Origin":"*",
+                headers: h 
+            }
+        )
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(jsonResponse => {
+            if (typeof jsonResponse === 'object' && jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories.alias,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                });
+            } else return [];
+        });
+    }
+}
+
+export default Yelp;
